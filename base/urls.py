69bc00d3be0fc37c1050e5bba81c45bc64add979
keerthana srinivas <ssku1246@gmail.com>
2026-09-12 19:57:45 +0530

    fix homepage routing

--- base/urls.py
+++ base/urls.py
@@ -1,21 +1,14 @@
-from django.urls import path
-from base import views
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    #Genre
-    path('create_genre/', views.create_genre, name='create_genre'),
-    path('read_genre/', views.read_genre, name='read_genre'),
-    path('update_genre/<int:pk>/', views.update_genre, name='update_genre'),
-    path('delete_genre/<int:pk>/', views.delete_genre, name='delete_genre'), 
-
-    #Article
-    path('home/', views.home, name='home'),
-    path('', views.read_article, name='read_article'),
-    path('update_article/<int:pk>/', views.update_article, name='update_article'),
-    path('delete_article/<int:pk>/', views.delete_article, name='delete_article'),
-    path('history_article/', views.history_article, name='history_article'),
-    path('restore_article/<int:pk>/', views.restore_article, name='restore_article'),
-
-    # <int:pk>/ ---> path converter
-       
+    path('admin/', admin.site.urls),
+    path('', include('base.urls')),
+    path('authen/', include('authen.urls')),
+    path('api/', include('api.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
